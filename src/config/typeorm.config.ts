56d1df3c337aefc0f232390a10dbaeeb74c00eac
Archaeologist03 <omniiknight@hotmail.com>
2020-05-07 16:45:19 +0200

    checking heroku

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,13 +3,19 @@ import * as config from 'config';
 
 const dbConfig = config.get('db');
 
+console.log(process.env.HEROKU_POSTGRESQL_ORANGE, dbConfig.database);
+console.log(process.env.DB_PASSWORD, dbConfig.password);
+console.log(process.env.DB_HOST, dbConfig.host);
+console.log(process.env.DB_USER, dbConfig.username);
+console.log(process.env.NODE_ENV);
+
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: dbConfig.type,
   host: process.env.DB_HOST || dbConfig.host,
   port: process.env.DB_PORT || dbConfig.port,
   username: process.env.DB_USER || dbConfig.username,
   password: process.env.DB_PASSWORD || dbConfig.password,
-  database: process.env.HEROKU_POSTGRESQL_ORANGE_URL || dbConfig.database,
+  database: process.env.HEROKU_POSTGRESQL_ORANGE || dbConfig.database,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
 };
