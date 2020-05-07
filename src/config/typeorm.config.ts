4e0ae3e8a241239b9bd6422d5a03e77587603ce6
Archaeologist03 <omniiknight@hotmail.com>
2020-05-07 21:17:41 +0200

    Heroku addin host env

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -8,7 +8,6 @@ console.log(process.env.DB_PASSWORD, dbConfig.password);
 console.log(process.env.DB_HOST, dbConfig.host);
 console.log(process.env.DB_USER, dbConfig.username);
 console.log(process.env.NODE_ENV);
-
 console.log(process.env.JWT_SECRET, 2323);
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
