e601074f90ed32fac00c61b869779f6b72bf2df3
Archaeologist03 <omniiknight@hotmail.com>
2020-05-07 12:29:12 +0200

    Applying logging - lection 62.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -11,6 +11,7 @@ import {
   ValidationPipe,
   ParseIntPipe,
   UseGuards,
+  Logger,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -25,6 +26,8 @@ import { GetUser } from 'src/auth/get-user.decorator';
 @Controller('tasks')
 @UseGuards(AuthGuard())
 export class TasksController {
+  private logger = new Logger('TaskController');
+
   constructor(private tasksService: TasksService) {}
 
   @Get()
@@ -32,6 +35,12 @@ export class TasksController {
     @Query(ValidationPipe) filterDto: GetTasksFilterDto,
     @GetUser() user: User,
   ) {
+    this.logger.verbose(
+      `User "${user.username}" retriving all tasks. Filters: ${JSON.stringify(
+        filterDto,
+      )}`,
+    );
+
     return this.tasksService.getTasks(filterDto, user);
   }
 
@@ -49,6 +58,12 @@ export class TasksController {
     @Body() createTaskDto: CreateTaskDto,
     @GetUser() user: User,
   ): Promise<Task> {
+    this.logger.verbose(
+      `User "${user.username}" creating a new task. Data: ${JSON.stringify(
+        createTaskDto,
+      )} `,
+    );
+
     return this.tasksService.createTask(createTaskDto, user);
   }
 
